5d558eb7ed12a923a374ac1e5190a77813804ec6
Max Zheng <maxzheng.os@gmail.com>
2023-10-23 11:54:09 -0700

    Fix typo

--- games/scripts.py
+++ games/scripts.py
@@ -5,6 +5,6 @@ from games.manager import Manager
 
 @click.command()
 @click.option('--fps', type=int, default=30, help='Set the frames per second to render')
-@click.option('--debug', is_flag=True, help='Turn on debug modoe')
+@click.option('--debug', is_flag=True, help='Turn on debug mode')
 def main(fps, debug):
     Manager().start(fps=fps, debug=debug)
