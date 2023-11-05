30780027fb04dfeeb4d213d7dc0a5873bf90a2e4
Max Zheng <maxzheng.os@gmail.com>
2023-11-04 19:01:06 -0700

    Add sanity tests for manager/controller

--- tests/test_games.py
+++ tests/test_games.py
@@ -1,2 +1,15 @@
-def test_games():
-    """ Test is code's best friend. ^_^ """
+import pytest
+
+from games.manager import Manager
+from games.controller import Controller
+from games.screen import Screen
+
+
+def test_manager():
+    Manager()
+
+
+def test_controller():
+    screen = Screen()
+    with pytest.raises(ValueError):
+        controller = Controller(screen)  # noqa
